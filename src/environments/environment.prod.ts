da4d438b805588eb71c3a938c27b0b60af59449a
lidia9319 <liamormu19@gmail.com>
2022-01-20 11:36:59 -0500

    Se agregan variables de entorno en produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  marvelAPI: {
+    timestamp: 1,
+    baseUrl: "https://gateway.marvel.com",
+    publicKey: "d1fb058f630a0ac4e7af3c6be085034c",
+    privateKey: "e56922f66c8abefdb629dea018f3a06239a415a2"
+  }
 };
